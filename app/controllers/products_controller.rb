62042529d9b4c5f7e49baade48153d605fb71ca4
mkaur124 <imuskansandhu04@gmail.com>
2025-11-25 20:49:40 -0600

    Add show route for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,24 @@
 class ProductsController < ApplicationController
+  # Show list of products, optionally filtered by category
   def index
     if params[:category].present?
-      # Filter products by selected category
       @products = Product.joins(:category)
                          .where(categories: { name: params[:category] })
                          .order(created_at: :desc)
                          .page(params[:page])
                          .per(20)
     else
-      # Show all products if no category selected
       @products = Product.order(created_at: :desc)
                          .page(params[:page])
                          .per(20)
+    end
+  end
+
+  # Show a single product
+  def show
+    @product = Product.find_by(id: params[:id])
+    unless @product
+      redirect_to products_path, alert: "Product not found"
     end
   end
 end
